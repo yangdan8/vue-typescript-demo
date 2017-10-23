7da345cdaf5a92765e670a118e07e0ee6c0cdef5
yangd <13532023084@139.com>
2017-10-24 07:32:50 +0800

    解决无法单独打包组件的bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-// /// <reference path="../../node_modules/@types/webpack-env/index.d.ts" />
+/// <reference path="../../node_modules/@types/webpack-env/index.d.ts" />
 
 import Vue from 'vue';
 import Router, { RouterOptions } from 'vue-router';
@@ -9,15 +9,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      // name: 'account_login',
+      name: 'account_login',
       components: {
         Login: (resolve) => {
-          // require.ensure([], (require) => {
-          //   const cmp = require('../components/account/login').default;
-          //   resolve(cmp);
-          // }, 'Login');
           require(['../components/account/login.ts'], resolve);
         }
+        // Login: (resolve) => {
+        //   require.ensure([], (require) => {
+        //     const cmp = require('../components/account/login').default;
+        //     resolve(cmp);
+        //   }, 'Login');
+        // }
+        // Login: () => require('../components/account/login.ts').default
       }
     }
   ]
