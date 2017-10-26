0b0b3ac7ae69a6c85f9e693a173c9d112ed73a8e
yangd <13532023084@139.com>
2017-10-26 21:39:30 +0800

    更新到最新版本的插件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,3 @@
-/// <reference path="../../node_modules/@types/webpack-env/index.d.ts" />
-
 import Vue from 'vue';
 import Router, { RouterOptions } from 'vue-router';
 const Login = () => import('../components/account/login');
